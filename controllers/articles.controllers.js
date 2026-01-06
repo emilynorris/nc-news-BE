cd9cd223516d116220c971e5edfd83e9d304cf4b
emilynorris <emilynorris2303@gmail.com>
2026-01-06 15:13:09 +0000

    Removed next(err) until error handling complete

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,18 +3,13 @@ const { fetchAllArticles , fetchArticlesById , updateArticleVotesById } = requir
 function getAllArticles (req,res) {
     fetchAllArticles().then((articles) => {
         res.status(200).send({articles})
-    }).catch((err) => {
-    next(err)
-  })
+    })
 }
 
 function getArticlesById (req,res,next) {
   const { article_id } = req.params
   fetchArticlesById(article_id).then((article) => {
     res.status(200).send({article})
-  })
-  .catch((err) => {
-    next(err)
   })
 }
 
